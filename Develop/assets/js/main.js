68d46c4c1bf6dec41a31b2ee50e38887f60fce3a
Michelle Wehr <michelle_wehr@icloud.com>
2021-08-15 21:52:40 -0400

    got tasks to loadgit add .

--- Develop/assets/js/main.js
+++ Develop/assets/js/main.js
@@ -1,5 +1,5 @@
 //get current day and date to show in the header
-const currentDay = moment().format('dddd MMMM Do, YYYY');
+const currentDay = moment().format("dddd MMMM Do, YYYY");
 $(".jumbotron").append(currentDay);
 // get current hour
 const currentHour = "11";
@@ -12,69 +12,56 @@ const timeElArr = Array.apply(null, timeEl);
 let tasks = [];
 
 //check to see if each timeEl is equal to, <, > and if it is add a class to the input that matches
-timeElArr.forEach(time => {
-    if (time.dataset.time === currentHour) {
-       $(time).closest(".row").find("input").addClass("present");
-    }
-    else if (time.dataset.time < currentHour) {
-        $(time).closest(".row").find("input").addClass("past");
-    } else {
-        $(time).closest(".row").find("input").addClass("future");
-    }
+timeElArr.forEach((time) => {
+  if (time.dataset.time === currentHour) {
+    $(time).closest(".row").find("input").addClass("present");
+  } else if (time.dataset.time < currentHour) {
+    $(time).closest(".row").find("input").addClass("past");
+  } else {
+    $(time).closest(".row").find("input").addClass("future");
+  }
 });
 
 //when click button- save task
-$(".btn").on("click", saveTask)
+$(".btn").on("click", saveTask);
 
 function saveTask() {
-    let taskToSave = {
-        task: $(this).parent().parent().children(".task").val(),
-        time: $(this).parent().parent().children(".time").attr("id")
-    };
+  let taskToSave = {
+    task: $(this).parent().parent().children(".task").val(),
+    time: $(this).parent().parent().children(".time").attr("id"),
+  };
 
-    tasks = localStorage.getItem('tasks');
+  tasks = localStorage.getItem("tasks");
 
-    if(tasks === null) {
-        tasks = [];
-        tasks.push(taskToSave);
-        saveTasks();
-    } else {
-        tasks = JSON.parse(tasks);
-        tasks.push(taskToSave);
-        saveTasks();
-    }
+  if (tasks === null) {
+    tasks = [];
+    tasks.push(taskToSave);
+    saveToLocalStor();
+  } else {
+    tasks = JSON.parse(tasks);
+    tasks.push(taskToSave);
+    saveToLocalStor(taskToSave);
+  }
 }
 
-//save tasks function 
-function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+//save tasks function
+function saveToLocalStor() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
 //load tasks
-function loadTasks() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    if(!tasks) {
-        return;
-    }
-    for (let i = 0; i < tasks.length; i++) {
-        if(tasks[i].time === $(".time").attr("id")) {
-            $(".time").parent().children(".task").textContent = tasks[i].task;
-        }
-    }
-    }
+function loadTasks(tasks) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (!tasks) {
+    return;
+  }
 
-    //load tasks
-function loadTasks() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    if(!tasks) {
-        return;
-    }
-    for (let i = 0; i < tasks.length; i++) {
-        if(tasks[i].time === $(".time").attr("id")) {
-            $(".time").parent().children(".task").textContent = tasks[i].task;
-        }
-    }
-    }
+  tasks.forEach((task) => {
+    $("#" + task.time)
+      .parent()
+      .children(".task")
+      .val(task.task);
+  });
+}
 
 loadTasks();
-
-
